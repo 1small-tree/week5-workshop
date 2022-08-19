ff2d4d81e6938a7a033239d6ada351cc5fec792c
1small-tree <104819100+1small-tree@users.noreply.github.com>
2022-08-19 21:04:20 +0930

    Update function-2-2.cpp

--- function-2-2.cpp
+++ function-2-2.cpp
@@ -1,22 +1,49 @@
-#include <iostream>
+#include<string.h>
+#include<string>
+#include<iostream>
+
 
 using namespace std;
 
-int max_sub_sum(int *nums,int length){
-    int n_count = 0;
-    int max = 0;
-    for(int i=0 ; i<length ; ++i){
-        int sum = 0;
-        if(nums[i]<0){
-            n_count++;
-        }
-        for(int j=0 ; j<length ; ++j){
-            sum +=nums[i+j];
-            if(sum>max){
-                max=sum;
-            }
-        }
-    }
-    return (n_count == length) ? 0 : max;
+int maximum_sum(int *nums, int length);
 
+int main()
+{
+int old_array[10] = { 31,-41,59,26,-53,58,97,-93,-23,-84 };
+cout << "Largest sum is : " << maximum_sum(&old_array[0], 10) << endl;
+return 0;
+}
+
+int maximum_sum(int *nums, int length)
+{
+int i = 0, currentMax = 0, prevMax = 0;
+
+for (; i < length; i++)
+{
+if (nums[i] + currentMax > 0)
+{
+if (nums[i] < 0)
+{
+if (currentMax > prevMax)
+prevMax = currentMax;
+
+currentMax += nums[i];
+}
+else
+{
+currentMax += nums[i];
+}
+}
+else
+{
+if(currentMax > prevMax)
+prevMax = currentMax;
+currentMax = 0;
+}
+}
+
+if (prevMax > currentMax)
+return prevMax;
+
+return currentMax;
 }
